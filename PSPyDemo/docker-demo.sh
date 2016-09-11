9122741fdb3b99d2c2611dfb378bb737415d0ffe
ramblingcookiemonster <ramblingcookiemonster@users.noreply.github.com>
2016-09-11 18:25:43 -0400

    update demo file

--- PSPyDemo/docker-demo.sh
+++ PSPyDemo/docker-demo.sh
@@ -1,17 +1,26 @@
 # Assumptions:
-# I've installed Docker and docker machine if necessary
-    # Windows: https://docs.docker.com/engine/installation/windows/
-        # Already have Hyper-V enabled?  You can still run VirtualBox (included in toolbox if you don't have it already):
-        # http://www.hanselman.com/blog/SwitchEasilyBetweenVirtualBoxAndHyperVWithABCDEditBootEntryInWindows81.aspx
-    # macOS:   https://docs.docker.com/engine/installation/mac/
+    # Install Docker or (if necessary) Docker Toolbox
+        # Windows: https://docs.docker.com/engine/installation/windows/
+            # Already have Hyper-V enabled?  You can still run VirtualBox (included in toolbox if you don't have it already):
+            # http://www.hanselman.com/blog/SwitchEasilyBetweenVirtualBoxAndHyperVWithABCDEditBootEntryInWindows81.aspx
+        # macOS:   https://docs.docker.com/engine/installation/mac/
 
-# If using docker machine, I've created a default machine
-   # After the install, 'Docker Quickstart Terminal' will create this for you
-   # Or you can create it manually...
+    # If using docker machine, create a default machine
+       # After the install, 'Docker Quickstart Terminal' will create this for you
+       # Or you can create it manually...
 
-# Build the docker image, run it
-docker build --no-cache=true -t wframe/pspydemo .
+    # Navigate to path with Dockerfile
+
+    # This might just be me, but this demo is pretty ugly on Windows 8.1 with Docker Toolbox...
+
+# Build the docker image - this takes a little while
+# Read the Dockerfile to see what's happening
+docker build -t wframe/pspydemo .
+
+# Kick off a container using the image we built. Should be fast!
 docker run -v ~/sc:/source -it wframe/pspydemo
+# On Windows, I skipped over mapping the folder:
+docker run -it wframe/pspydemo
 
 # We're in a container!
 # Activate the pyvenv virtual environment...
